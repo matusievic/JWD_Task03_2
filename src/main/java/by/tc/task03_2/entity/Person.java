669c177ccf62b2108720fed20bfd7019c1f89ebc
Andrei Matusevich <matusievic@gmail.com>
2017-11-12 23:31:07 +0300

    Person class was updated equals(Object obj), hascode() and toString() methods were added

--- src/main/java/by/tc/task03_2/entity/Person.java
+++ src/main/java/by/tc/task03_2/entity/Person.java
@@ -44,4 +44,33 @@ public class Person implements Serializable {
     public void setEmail(String email) {
         this.email = email;
     }
+
+    @Override
+    public boolean equals(Object obj) {
+        if (this == obj) { return true; }
+        if (obj == null || getClass() != obj.getClass()) { return false; }
+
+        Person person = (Person) obj;
+
+        if (name != null ? !name.equals(person.name) : person.name != null) { return false; }
+        if (surname != null ? !surname.equals(person.surname) : person.surname != null) { return false; }
+        if (phone != null ? !phone.equals(person.phone) : person.phone != null) { return false; }
+        if (email != null ? !email.equals(person.email) : person.email != null) { return false; }
+
+        return true;
+    }
+
+    @Override
+    public int hashCode() {
+        double result = name != null ? name.hashCode() : 0;
+        result = 31 * result + (surname != null ? surname.hashCode() : 0);
+        result = 31 * result + (phone != null ? phone.hashCode() : 0);
+        result = 31 * result + (email != null ? email.hashCode() : 0);
+        return (int) result;
+    }
+
+    @Override
+    public String toString() {
+        return getClass().getName() + " [name=" + name + ", surname=" + surname + ", phone=" + phone + ",email=" + email;
+    }
 }

7f141af3a13689571a322f6680efe6ed51a1ca16
Andrei Matusevich <matusievic@gmail.com>
2017-11-13 02:28:54 +0300

    Services were updated

--- src/main/java/by/tc/task03_2/service/PersonService.java
+++ src/main/java/by/tc/task03_2/service/PersonService.java
@@ -2,6 +2,8 @@ package by.tc.task03_2.service;
 
 import by.tc.task03_2.entity.Person;
 
+import java.sql.SQLException;
+
 public interface PersonService {
-    Person[] find(String name, String surname);
+    Person[] find(String name, String surname) throws SQLException, IllegalArgumentException;
 }
--- src/main/java/by/tc/task03_2/service/impl/PersonServiceImpl.java
+++ src/main/java/by/tc/task03_2/service/impl/PersonServiceImpl.java
@@ -4,16 +4,18 @@ import by.tc.task03_2.dao.DAOFactory;
 import by.tc.task03_2.dao.PersonDAO;
 import by.tc.task03_2.entity.Person;
 import by.tc.task03_2.service.PersonService;
+import by.tc.task03_2.service.validation.Validator;
+
+import java.sql.SQLException;
 
 public class PersonServiceImpl  implements PersonService {
     @Override
-    public Person[] find(String name, String surname) {
-        if (name == null || name.isEmpty()) { return null; }
-        if (surname == null || surname.isEmpty()) { return null; }
+    public Person[] find(String name, String surname) throws SQLException, IllegalArgumentException {
+        if (!Validator.nameValidator(name, surname)) { throw new IllegalArgumentException("Illegal name or surname"); }
 
         PersonDAO personDAO = DAOFactory.getInstance().getPersonDAO();
-        Person[] persones = personDAO.find(name, surname);
+        Person[] persons = personDAO.find(name, surname);
 
-        return persones;
+        return persons;
     }
 }

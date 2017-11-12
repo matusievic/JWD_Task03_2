0600c265d4bd9c30761cebc7f507e9e11b9cda80
Andrei Matusevich <matusievic@gmail.com>
2017-11-13 02:00:29 +0300

    DAO classes were updated Database reading was added

--- src/main/java/by/tc/task03_2/dao/PersonDAO.java
+++ src/main/java/by/tc/task03_2/dao/PersonDAO.java
@@ -2,6 +2,8 @@ package by.tc.task03_2.dao;
 
 import by.tc.task03_2.entity.Person;
 
+import java.sql.SQLException;
+
 public interface PersonDAO {
-    Person[] find(String name, String surname);
+    Person[] find(String name, String surname) throws SQLException;
 }
--- src/main/java/by/tc/task03_2/dao/impl/PersonDAOImpl.java
+++ src/main/java/by/tc/task03_2/dao/impl/PersonDAOImpl.java
@@ -3,9 +3,42 @@ package by.tc.task03_2.dao.impl;
 import by.tc.task03_2.dao.PersonDAO;
 import by.tc.task03_2.entity.Person;
 
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.ArrayList;
+import java.util.List;
+
 public class PersonDAOImpl implements PersonDAO {
+    private static final String URL = "jdbc:mysql://localhost:3306/persones";
+    private static final String USER = "root";
+    private static final String PASSWORD = "root";
+
     @Override
-    public Person[] find(String name, String surname) {
-        return new Person[0];
+    public Person[] find(String name, String surname) throws SQLException {
+        Connection connection = DriverManager.getConnection(URL, USER, PASSWORD);
+        PreparedStatement statement = connection.prepareStatement("SELECT *FROM Persones WHERE NAME=? AND SURNAME=?");
+        statement.setString(1, name);
+        statement.setString(2, surname);
+        ResultSet resultSet = statement.executeQuery();
+        statement.clearParameters();
+
+        List<Person> personList = new ArrayList<>();
+
+        while (resultSet.next()) {
+            Person person = new Person();
+
+            person.setName(resultSet.getString("NAME"));
+            person.setSurname(resultSet.getString("SURNAME"));
+            person.setPhone(resultSet.getString("PHONE"));
+            person.setEmail(resultSet.getString("EMAIL"));
+
+            personList.add(person);
+        }
+
+        Person[] result = new Person[personList.size()];
+        return personList.toArray(result);
     }
 }

03245e1e1af39fbafc2436b5ff8fc6190823e243
Andrei Matusevich <matusievic@gmail.com>
2017-11-13 02:38:52 +0300

    ServiceFactory class was updated

--- src/main/java/by/tc/task03_2/service/ServiceFactory.java
+++ src/main/java/by/tc/task03_2/service/ServiceFactory.java
@@ -1,7 +1,10 @@
 package by.tc.task03_2.service;
 
+import by.tc.task03_2.service.impl.PersonServiceImpl;
+
 public final class ServiceFactory {
     private static final ServiceFactory instance = new ServiceFactory();
+    private final PersonService personService = new PersonServiceImpl();
 
     private ServiceFactory() {
 
@@ -9,5 +12,9 @@ public final class ServiceFactory {
 
     public static ServiceFactory getInstance() {
         return instance;
+    }
+
+    public PersonService getPersonService() {
+        return personService;
     }
 }

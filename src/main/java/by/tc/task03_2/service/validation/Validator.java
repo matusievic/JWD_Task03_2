4e21f8859ebb89d9bdd2a6097abf2671860a0321
Andrei Matusevich <matusievic@gmail.com>
2017-11-13 02:25:15 +0300

    Validator class was updated

--- src/main/java/by/tc/task03_2/service/validation/Validator.java
+++ src/main/java/by/tc/task03_2/service/validation/Validator.java
@@ -1,7 +1,10 @@
 package by.tc.task03_2.service.validation;
 
 public final class Validator {
+    private static final String NAME_REGEX = "[A-Z][a-z]+";
     public static boolean nameValidator(String name, String surname) {
+        if (name == null || name.isEmpty() || !name.matches(NAME_REGEX)) { return false; }
+        if (surname == null || surname.isEmpty() || !surname.matches(NAME_REGEX)) { return false; }
         return true;
     }
 }
